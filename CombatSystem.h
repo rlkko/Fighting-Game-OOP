6f9b0628317a408804d6eb8903ad5f794a6c8291
rikkoRicardo <ricardofonseca673@gmail.com>
2021-11-09 22:23:01 +0000

    Update CombatSystem.h

--- CombatSystem.h
+++ CombatSystem.h
@@ -3,11 +3,10 @@
 #include "Player.h"
 
 namespace CombatSystem {
-	void HitLog(Player& assasin, Player& victim, int rand_attack);
 	void AddDefaultPlayer();
 	void AddPlayer(std::string name, short hp, unsigned short attack, unsigned short defense);
-	void HitLog(Player& assasin, Player& victim, int rand_attack);
-	void DefendLog(Player& assasin, Player& victim, int rand_attack);
+	void HitLog(Player& fighter, Player& victim, int rand_attack);
+	void DefendLog(Player& fighter, Player& victim, int rand_attack);
 	void InitLog();
 	bool CheckWinner();
 	void Fight(int attack_value, int defense_value, Player& fighter, Player& victim);
