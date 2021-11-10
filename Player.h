3537ddc36912aaff9ac65eb8078cd7500540f10e
rikkoRicardo <ricardofonseca673@gmail.com>
2021-11-10 00:01:28 +0000

    Update Player.h

--- Player.h
+++ Player.h
@@ -4,7 +4,7 @@
 class Player
 {
 private:
-	const std::string name;
+	std::string name;
 	int16_t hp;
 	uint16_t attack;
 	uint16_t defense;
@@ -12,20 +12,20 @@ private:
 
 public:
 
-	constexpr short getRandAttack() const { return rand() % this->attack; }
-	constexpr short getRandDefense() const { return rand() % this->defense; }
+	short getRandAttack() const { return rand() % this->attack; }
+	short getRandDefense() const { return rand() % this->defense; }
 
-	Player() {Player DefaultPlayer(this->name = "Default", this->hp = 100, this->attack = 20, this->defense = 30, this->luck = 0.2f);}
+	Player() { Player DefaultPlayer(this->name = "Default", this->hp = 100, this->attack = 20, this->defense = 30, this->luck = 0.2f); }
 
 	//Define a costum player model
-	Player(const std::string theName = "Non", int16_t theHp= 100, uint16_t theAttack = 20, uint16_t theDefense =30, float theLuck = 0.2f) :name(theName), hp(theHp), attack(theAttack), luck(theLuck) { }
+	Player(const std::string theName, int16_t theHp, uint16_t theAttack, uint16_t theDefense, float theLuck) :name(theName), hp(theHp), attack(theAttack),defense(theDefense), luck(theLuck) { }
 
 	short getHit(uint16_t Enemydmg) { return this->hp -= Enemydmg; }
 
 	constexpr bool isDead() const { return (this->hp <= 0 ? true : false); } // condition ? result_if_true : result_if_false
 
 	//Creating getters for the attributes so we can use them
-	constexpr std::string getName() const { return this->name; }
+	std::string getName() const { return this->name; }
 	constexpr short getHp() const { return this->hp; }
 	constexpr short getAttackInfo() const { return this->attack; }
 	constexpr short getDefenseInfo() const { return this->defense; }
